e9352c8274ce24d0c029b94fd851176de597a66a
baochi0212 <chideptraiak29@gmail.com>
2023-01-17 12:17:52 +0700

    get block data

--- src/data/spam_script.sh
+++ src/data/spam_script.sh
@@ -1,11 +1,12 @@
 n_spams=$1 
 start=$2
-type=$3
-end=$4
+end=$3
+type=$4
 echo "Num steps: ${n_spams}"
 for ((i = 0; i <= $n_spams; i++));
 do
-    echo "Step: ${i}" 
+    echo "Step: ${i}, type: ${type}" 
+    echo "Limit: ${start} -> ${end}"
     python src/data/get_block.py --start $start \
      --end $end \
      --type $type \
