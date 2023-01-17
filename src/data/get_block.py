732cdf0c974831f93f639af2ca9bdd6d2504cd8b
baochi0212 <chideptraiak29@gmail.com>
2023-01-17 13:10:38 +0700

    get block data

--- src/data/get_block.py
+++ src/data/get_block.py
@@ -25,7 +25,7 @@ def getHash(start, end, attack_file='timestamp.csv'):
 
 
         #save block hash of dates in metadata according to category
-        save_file = f"{meta_dir}/{cat}.json" if init else f"{meta_dir}/temp.json"
+        save_file = f"{meta_dir}/{cat}_{start}_{end}.json" if init else f"{meta_dir}/temp.json"
         time_api = f"https://blockchain.info/blocks/{timestamp}?format=json"
         os.system(f"curl --http 1.1 {time_api} > {save_file}")
         #merge to main cat.json file
